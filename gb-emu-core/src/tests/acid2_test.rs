3d39f4bd60788d93d4238e9357da9c2adaca9a4d
Amjad Alsharafi <amjadsharafi10@gmail.com>
2021-01-11 14:51:52 +0300

    Testing: Added cgb-acid2 test
    source: https://github.com/mattcurrie/cgb-acid2

--- gb-emu-core/src/tests/acid2_test.rs
+++ gb-emu-core/src/tests/acid2_test.rs
@@ -1,7 +1,11 @@
 gb_tests!(
     brk; // clock until break
 
-    acid2_test,
+    dmg_acid2_test,
     "dmg-acid2.gb",
     4534603639967981476;
+
+    cgb_acid2_test,
+    "cgb-acid2.gbc",
+    4378550468433865064;
 );
